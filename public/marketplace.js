cad25c1b7b1839ea7166a198a327ea660b174335
Eduard Brahas <eduardbrhas@outlook.it>
2024-05-28 10:45:02 +0200

    [ADD] search for courses logic

--- public/marketplace.js
+++ public/marketplace.js
@@ -1,47 +1,107 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    try {
-      if(document.getElementById('marketplace-title') !== null) {
-        const response = await fetch('/marketplace-courses');
-        const data = await response.json();
-  
-        const cardbox = document.getElementById('cardbox');
-  
-        data.courses.forEach(course => {
-          const card = document.createElement('div');
-          card.className ='cards';
-  
-          const img = document.createElement('img');
-          img.src = course.thumbnail_path;
-          img.alt ='lesson image';
-          img.id ='card-img';
-  
-          const p = document.createElement('p');
-          p.textContent = course.title;
-          p.className ='card-title';
-  
-          const div_button = document.createElement('div');
-          div_button.className = 'button-container';
-  
-          const button_desc = document.createElement('button');
-          button_desc.textContent ='Description';
-          button_desc.className ='subscribe-btn';
-          button_desc.onclick = "openDescription()";
-  
-          button_subscribe = document.createElement('button');
-          button_subscribe.textContent ='Subscribe';
-          button_subscribe.className ='subscribe-btn';
-  
-          div_button.appendChild(button_desc);
-          div_button.appendChild(button_subscribe);
-  
-          card.appendChild(img);
-          card.appendChild(p);
-          card.appendChild(div_button);
-  
-          cardbox.append(card);
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    if (document.getElementById("marketplace-title") !== null) {
+      const response = await fetch("/marketplace-courses");
+      const data = await response.json();
+
+      const cardbox = document.getElementById("cardbox");
+
+      data.courses.forEach((course) => {
+        const card = document.createElement("div");
+        card.className = "cards";
+
+        const img = document.createElement("img");
+        img.src = course.thumbnail_path;
+        img.alt = "lesson image";
+        img.id = "card-img";
+
+        const p = document.createElement("p");
+        p.textContent = course.title;
+        p.className = "card-title";
+
+        const div_button = document.createElement("div");
+        div_button.className = "button-container";
+
+        const button_desc = document.createElement("button");
+        button_desc.textContent = "Description";
+        button_desc.className = "subscribe-btn";
+        button_desc.onclick = "openDescription()";
+
+        button_subscribe = document.createElement("button");
+        button_subscribe.textContent = "Subscribe";
+        button_subscribe.className = "subscribe-btn";
+
+        div_button.appendChild(button_desc);
+        div_button.appendChild(button_subscribe);
+
+        card.appendChild(img);
+        card.appendChild(p);
+        card.appendChild(div_button);
+
+        cardbox.append(card);
+      });
+
+      document
+        .getElementById("finder")
+        .addEventListener("input", async function () {
+          const query = this.value;
+
+          const response = await fetch("/marketplace-search", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ query: query }),
+          });
+
+          const courses = await response.json();
+
+          // Update the UI with the search results
+          updateCourseCards(courses);
         });
-      }
-    } catch (error) {
-      console.error('Error fetching courses:', error);
     }
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+  }
+});
+
+function updateCourseCards(courses) {
+  const cardbox = document.getElementById("cardbox");
+  cardbox.innerHTML = ""; // Clear previous results
+  courses.forEach((course) => {
+    const card = document.createElement("div");
+    card.className = "cards";
+
+    const img = document.createElement("img");
+    img.src = course.thumbnail_path;
+    img.alt = "lesson image";
+    img.id = "card-img";
+
+    const p = document.createElement("p");
+    p.textContent = course.title;
+    p.className = "card-title";
+
+    const div_button = document.createElement("div");
+    div_button.className = "button-container";
+
+    const button_desc = document.createElement("button");
+    button_desc.textContent = "Description";
+    button_desc.className = "subscribe-btn";
+
+    button_desc.onclick = function () {
+      openDescription(course.descr);
+    };
+    const button_subscribe = document.createElement("button");
+    button_subscribe.textContent = "Subscribe";
+    button_subscribe.className = "subscribe-btn";
+
+    div_button.appendChild(button_desc);
+    div_button.appendChild(button_subscribe);
+
+    card.appendChild(img);
+    card.appendChild(p);
+    card.appendChild(div_button);
+
+    cardbox.appendChild(card);
   });
+}
--- public/script.js
+++ public/script.js
@@ -1,28 +1,27 @@
 // Fetch session data from the server
-fetch('/session-data')
-.then(response => response.json())
-.then(data => {
+fetch("/session-data")
+  .then((response) => response.json())
+  .then((data) => {
     if (data.isAuth) {
-      document.getElementById('1-entry').href = '/settings';
-      document.getElementById('1-entry').textContent = 'User';
-      
-      document.getElementById('username').textContent = data.username;
-      document.getElementById('bio').textContent = data.bio;
-      if(data.msgToUser){
-        document.getElementById('msgToUser').textContent = data.msgToUser;
+      document.getElementById("1-entry").href = "/settings";
+      document.getElementById("1-entry").textContent = "User";
+
+      document.getElementById("username").textContent = data.username;
+      document.getElementById("bio").textContent = data.bio;
+      if (data.msgToUser) {
+        document.getElementById("msgToUser").textContent = data.msgToUser;
       }
-
     } else {
-      document.getElementById('1-entry').href = '/signup';
-      document.getElementById('1-entry').textContent = 'Signup';
+      document.getElementById("1-entry").href = "/signup";
+      document.getElementById("1-entry").textContent = "Signup";
 
-      var entry = document.createElement('a');
-      entry.id ='log-entry';
-      entry.href = '/login';
-      entry.textContent = 'Login';
-      document.getElementById('nav-link').append(entry);
-      if(data.msgToUser){
-        document.getElementById('msgToUser').textContent = data.msgToUser;
+      var entry = document.createElement("a");
+      entry.id = "log-entry";
+      entry.href = "/login";
+      entry.textContent = "Login";
+      document.getElementById("nav-link").append(entry);
+      if (data.msgToUser) {
+        document.getElementById("msgToUser").textContent = data.msgToUser;
       }
     }
-});
+  });
