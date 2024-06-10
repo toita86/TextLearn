2d2ff6b6962aef5a7e80d66a648a526bc7e8254a
Eduard Brahas <eduardbrhas@outlook.it>
2024-06-10 10:13:47 +0200

    [FIX] front_side.js bug

--- public/front_side.js
+++ public/front_side.js
@@ -1,69 +1,69 @@
-document.addEventListener("DOMContentLoaded", () =>  {
-
-  /*handleResize è chiamata con due eventi: quando c'è il ridimensionamento della finestra 
+/*handleResize è chiamata con due eventi: quando c'è il ridimensionamento della finestra 
   e quando ce il caricamenro della pagina per fare in modo che quando la pagina si 
   allarga ritornano i classici navlinks
   innerwith erestituisce il valore della larghezza della pagina in un certo momento 
   in pixel
   */
-  function handleResize() {
-    const navLinkMini = document.getElementById("nav-link-mini");
-    const navLink = document.getElementById("nav-link");
-    const hamburger = document.getElementById("hamburger");
-    const xButton = document.getElementById("x_button");
+function handleResize() {
+  const navLinkMini = document.getElementById("nav-link-mini");
+  const navLink = document.getElementById("nav-link");
+  const hamburger = document.getElementById("hamburger");
+  const xButton = document.getElementById("x_button");
 
-    if (window.innerWidth > 1080) {
+  if (window.innerWidth > 1080) {
+    navLinkMini.style.display = "none";
+    navLink.style.display = "flex";
+    hamburger.style.display = "none";
+    xButton.style.display = "none";
+  } else {
+    navLink.style.display = "none";
+    if (xButton.style.display === "none") {
+      hamburger.style.display = "block";
       navLinkMini.style.display = "none";
-      navLink.style.display = "flex";
-      hamburger.style.display = "none";
-      xButton.style.display = "none";
-    } else {
-      navLink.style.display = "none";
-      if (xButton.style.display === "none") {
-        hamburger.style.display = "block";
-        navLinkMini.style.display = "none";
-      }
     }
   }
+}
 
-  // Chiamare handleResize quando la finestra viene ridimensionata
-  window.addEventListener("resize", handleResize);
+// Chiamare handleResize quando la finestra viene ridimensionata
+window.addEventListener("resize", handleResize);
 
-  // Chiamare handleResize quando la pagina viene caricata
-  window.addEventListener("load", handleResize);
+// Chiamare handleResize quando la pagina viene caricata
+window.addEventListener("load", handleResize);
 
-  // GESRIONE DEL MENU AD HAMBURGER
-  function openNav() {
-    navlink = document.getElementById("nav-link-mini");
-    if (document.getElementById("hamburger").style.display !== "none") {
-      navlink.style.display = "flex";
-      navlink.style.animation = "slideRight 0.5s";
-      document.getElementById("x_button").style.display = "block";
-      document.getElementById("hamburger").style.display = "none";
-    } else if (document.getElementById("x_button").style.display !== "none") {
-      navlink.style.animation = "slideLeft 0.5s";
-      setTimeout(() => {
-        navlink.style.display = "none";
-      }, 300); // Wait for the slideLeft animation to finish
-      document.getElementById("x_button").style.display = "none";
-      document.getElementById("hamburger").style.display = "block";
-    }
+// GESRIONE DEL MENU AD HAMBURGER
+function openNav() {
+  navlink = document.getElementById("nav-link-mini");
+  if (document.getElementById("hamburger").style.display !== "none") {
+    navlink.style.display = "flex";
+    navlink.style.animation = "slideRight 0.5s";
+    document.getElementById("x_button").style.display = "block";
+    document.getElementById("hamburger").style.display = "none";
+  } else if (document.getElementById("x_button").style.display !== "none") {
+    navlink.style.animation = "slideLeft 0.5s";
+    setTimeout(() => {
+      navlink.style.display = "none";
+    }, 300); // Wait for the slideLeft animation to finish
+    document.getElementById("x_button").style.display = "none";
+    document.getElementById("hamburger").style.display = "block";
   }
+}
 
-  const results = document.getElementsByClassName("course");
-  if (results.length > 0) {
-    /*mostra il titiolo del corso a cui sono iscritto o creato al passaggio del mouse come alert*/
-    course = document.getElementsByClassName("course");
-    course.getElementsById("course-title").addEventListener("hover", function () {
-      alert(this.textContent);
-    });
-  }
-
-  if (document.getElementById("go-up-button")){
+const results = document.getElementsByClassName("course");
+if (results.length > 0) {
+  /*mostra il titiolo del corso a cui sono iscritto o creato al passaggio del mouse come alert*/
+  course = document.getElementsByClassName("course");
+  course.getElementsById("course-title").addEventListener("hover", function () {
+    alert(this.textContent);
+  });
+}
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.getElementById("go-up-button")) {
     //Go Up button
-    document.getElementById("go-up-button").addEventListener("click", function () {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    });
+    document
+      .getElementById("go-up-button")
+      .addEventListener("click", function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      });
   }
 });
